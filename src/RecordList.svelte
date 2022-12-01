074258560c632bcbc26dee8455c0ec67a7ffc19e
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-01 18:02:45 +0100

    Add loading of usagi CSVs and basic table
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

---
+++ src/RecordList.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { PowerTable } from '@muonw/powertable';
+	import type { Options } from '@muonw/powertable/package/components/PowerTable.svelte';
+
+	export var recordList: any[] = [];
+	var ptOptions: Options = {
+		footerFilters: false,
+		footerLoadingBar: false,
+		headerLoadingBar: false,
+		isDataRemote: false	
+	}
+</script>
+
+<PowerTable
+    ptData={recordList}
+    on:rowClicked="{(d) => console.log('click', d)}"
+    on:rowDblClicked="{(d) => console.log('dblclick', d)}"
+	ptOptions={ptOptions}
+/>
+
+<style>
+	@import '../../node_modules/@muonw/powertable/package/dist/power-table.css';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,25 @@
 <script lang="ts">
-	function dropHandler(ev: DragEvent) {
-		console.log(typeof ev);
-		console.log('File(s) dropped');
+	//dependencies
+	import prettyBytes from 'pretty-bytes';
+    //elements
+	import FileDropZone from "../FileDropZone.svelte";
+	import CsvLoadingScreen from '../CsvLoadingScreen.svelte';
+	import RecordList from '../RecordList.svelte';
 
-		// Prevent default behavior (Prevent file from being opened)
-		ev.preventDefault();
+	export let selectedCsv: File;
+	export let csvRecords: any[] = [];
 
-		if (ev.dataTransfer?.items) {
-			// Use DataTransferItemList interface to access the file(s)
-			[...ev.dataTransfer.items].forEach((item, i) => {
-				// If dropped items aren't files, reject them
-				if (item.kind === 'file') {
-					const file = item.getAsFile();
-					console.log(`… file[${i}].name = ${file?.name}`);
-				}
-			});
-		} else {
-			// Use DataTransfer interface to access the file(s)
-            ev.dataTransfer?.items.
-			[...ev.dataTransfer?.files!].forEach((file, i) => {
-				console.log(`… file[${i}].name = ${file.name}`);
-			});
-		}
-	}
-	function dragOverHandler(ev: Event) {
-		console.log('File(s) in drop zone');
-
-		// Prevent default behavior (Prevent file from being opened)
-		ev.preventDefault();
-	}
+	let isLoaded: boolean = false;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div id="drop_zone" on:drop={dropHandler} on:dragover={dragOverHandler}>
-	<p>Drag one or more files to this <i>drop zone</i>.</p>
-</div>
+<h1>Keun</h1>
+<p></p>
 
-<style>
-	#drop_zone {
-		border: 5px solid blue;
-		width: 200px;
-		height: 100px;
-	}
-</style>
+{#if !selectedCsv}
+	<FileDropZone requiredFileExtension=".csv" bind:selectedFile={selectedCsv} text="Drag a Usagi-compatible CSV file into this box!" />
+{:else if !isLoaded}
+	<p>Selected file: {selectedCsv.name} ({prettyBytes(selectedCsv.size)})</p>
+	<CsvLoadingScreen selectedFile={selectedCsv} bind:recordList={csvRecords} bind:isLoaded={isLoaded} />
+{:else}
+	<RecordList recordList={csvRecords} />
+{/if}
