9c828040a3c2290d689ed4717070d7adb6d9af50
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-08 18:01:37 +0100

    Small refactor to csv parsers, add csv-parse based parser
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- src/CsvLoadingScreenCsvParse.svelte
+++ src/CsvLoadingScreenCsvParse.svelte
@@ -1,38 +1,23 @@
 <script lang="ts">
 	//dependencies
 	import prettyBytes from 'pretty-bytes';
+	import {parse} from 'csv-parse/browser/esm/sync';
 	export var selectedFile: File;
 	var downloadDebug = false;
-	var identifier = "csvParse.legacy."+new Date().getTime();
+	var identifier = "csvParse.csvparse."+new Date().getTime();
 
 	performance.mark(identifier+'.start')
 	var fileContents: string;
 	selectedFile.text().then((_text) => {
 		console.log('File selected, reading...');
 		//load file
-		fileContents = _text;
-		var rows = fileContents.split('\n');
-
-		//parse file
-		var header = rows[0].split(',');
-		rows.slice(1).forEach((row, i) => {
-			//REPLACE ME SOMEDAY: this assumes no commas in the data
-			var cells = row.split(',');
-			var obj: any = {};
-			header.forEach((col, j) => {
-				obj[col] = cells[j];
-			});
-			recordList.push(obj);
-			loadedRows = recordList.length;
-		});
+		recordList = parse(_text, {});
 		console.log(`Loaded ${recordList.length} rows...`);
 		//debug
 		isLoaded = true;
 		performance.mark(identifier+'.end')
 		performance.measure(identifier, identifier+'.start', identifier+'.end')
 		console.log({parseTime: `${performance.getEntriesByName(identifier)[0].duration} ms`, name: performance.getEntriesByName(identifier)[0].name, perf: performance.getEntriesByName(identifier)});
-		loadedRows = rows.length;
-
 	});
 	export var recordList: any[] = [];
 	export var isLoaded: boolean = false;
