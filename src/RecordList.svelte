d77aef2bc65ed8da54691cf0a335a01c462a0d3b
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-22 17:08:08 +0100

    Reduce data sent to powertable to improve render perf
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- src/RecordList.svelte
+++ src/RecordList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	export var verbose: boolean = false;
 	import { PowerTable } from '@muonw/powertable';
-	import type { Options } from '@muonw/powertable/package/components/PowerTable.svelte';
+	import type { DataFeed, Options } from '@muonw/powertable/package/components/PowerTable.svelte';
 	// import '@muonw/powertable/package/dist/power-table.css'; //file doesnt seem to exist?
 
 	export var recordList: any[] = [];
@@ -9,16 +10,34 @@
 		footerLoadingBar: false,
 		headerLoadingBar: false,
 		footerText: false,
-		isDataRemote: false,
-		
+		verbose: true,
+
+		isDataRemote: true,
+		userFunctions: {
+			dataFeed: async (data: any): Promise<DataFeed> => {
+				console.log('dataFeed', data);
+				const start = (data.options!.currentPage??1 - 1) * data.options!.rowsPerPage!;
+				const count = data.options!.rowsPerPage!;
+				console.log(`Request for ${count} rows, starting at ${start}`);
+				console.log(JSON.parse(JSON.stringify(data)));
+				console.log(data.data);
+			
+				//data.data = recordList.slice(start, start + count);
+				data.options!.totalRows = recordList.length;
+				console.log(JSON.parse(JSON.stringify(data)))
+				console.log(data.data);
+				return data;
+			},
+		}
 	};
 
 	
 </script>
 
 <div class="PowerTable">
+	<!-- 
+		ptData={recordList} -->
 	<PowerTable
-		ptData={recordList}
 		on:rowClicked={(d) => console.log('click', d)}
 		on:rowDblClicked={(d) => console.log('dblclick', d)}
 		{ptOptions}
