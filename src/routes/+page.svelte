3051a20fad2e04948116c784e5a236b45cb0d546
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-22 09:09:22 +0100

    Add benchmarks
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
 	//config
 	let isDebug = true;
-	let isBenchmarking: boolean = true;
+	let isBenchmarking: boolean = false;
 
 	//dependencies
 	import prettyBytes from 'pretty-bytes';
 	import { browser } from '$app/environment';
 
 	//elements
-	var _parser;
 	import FileDropZone from '../FileDropZone.svelte';
-	import CsvLoadingScreen from '../CsvLoadingScreen.svelte';
 	import CsvLoadingScreenPapaParse from '../CsvLoadingScreenPapaParse.svelte';
 	import RecordList from '../RecordList.svelte';
-	import CsvLoadingScreenCsvParse from '../CsvLoadingScreenCsvParse.svelte';
+
+	import CsvLoadingBenchmark from '../CsvLoadingBenchmark.svelte';
 
 	export let selectedFile: File;
 	export let recordList: any[] = [];
@@ -23,21 +22,9 @@
 	//debug
 	let debugInfo: string[] = [];
 
-	let benchmarkResults: any[] = [];
 	let memoryUsage: any = 0;
 	if (isBenchmarking)
 		setInterval(() => {
-			benchmarkResults = performance
-				.getEntries()
-				.filter((x) => x.entryType == 'mark' && x.name.endsWith('.end'))
-				.map((x) => ({
-					name: x.name.replace('.end', ''),
-					duration: performance.measure(
-						x.name.replace('.end', ''),
-						x.name.replace('.end', '.start'),
-						x.name
-					).duration
-				}));
 			// ignore error, chrome-only api, does not break compatibility with firefox
 			memoryUsage = browser && prettyBytes(performance.memory?.usedJSHeapSize);
 			debugInfo = [];
@@ -45,12 +32,6 @@
 				`Memory usage: ${memoryUsage}, browser: ${browser}, ${recordList.length} records`
 			);
 		}, 100);
-
-	let loaderTypes = [
-		{ name: 'Simple (string based, sync)', component: CsvLoadingScreen },
-		{ name: 'PapaParse (stream, sync)', component: CsvLoadingScreenPapaParse },
-		{ name: 'CSV-Parse (string, sync)', component: CsvLoadingScreenCsvParse }
-	];
 </script>
 
 <h1>Keun</h1>
@@ -68,42 +49,16 @@
 {:else if !isLoaded || isBenchmarking}
 	<p>Selected file: {selectedFile.name} ({prettyBytes(selectedFile.size)})</p>
 	{#if isBenchmarking}
-		<h1>
-			Benchmarking...
-			<img
-				src="https://media.tenor.com/1Txaw86RmuUAAAAj/bunny.gif"
-				alt="Bunny almost sleeping used as loading animation"
-				style="height: 1em;"
-			/>
-		</h1>
-		{#each benchmarkResults as result}
-			<p>{result.name} took {result.duration} ms</p>
-		{/each}
-		{#each loaderTypes as type}
-			<h1>{type.name}</h1>
-			{#each Array(1) as _}
-				<svelte:component this={type.component} {selectedFile} />
-			{/each}
-		{/each}
+		<CsvLoadingBenchmark {selectedFile} />
 	{:else}
+		<script>
+			console.log('[Flow] Loading csv...');
+		</script>
 		<CsvLoadingScreenPapaParse {selectedFile} bind:recordList bind:isLoaded />
 	{/if}
 {:else}
-	<div id="row1">
-		<RecordList {recordList} />
-	</div>
-	<div id="row2">
-		<p>Row 2</p>
-	</div>
-	<style>
-		#row1 {
-			grid-area: row1;
-		}
-		#row2 {
-			grid-area: row2;
-		}
-		#row1,
-		#row2 {
-		}
-	</style>
+	<script>
+		console.log('[Flow] Showing record list...');
+	</script>
+	<RecordList {recordList} />
 {/if}
