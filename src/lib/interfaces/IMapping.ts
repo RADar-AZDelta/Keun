a0d5cb940dc3e66eca56319c0a56d0730c968198
BostoenToby <toby.bostoen@student.howest.be>
2023-03-10 12:13:21 +0100

    Add author for mapping

--- src/lib/interfaces/IMapping.ts
+++ src/lib/interfaces/IMapping.ts
@@ -1,6 +1,7 @@
 export default interface IMapping {
-    row: number;
-    columns: any;
-    data: [string, any][],
-    equivalence: string;
+  row: number
+  columns: any
+  data: [string, any][]
+  equivalence: string
+  author: string
 }
