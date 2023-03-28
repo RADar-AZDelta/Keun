aa992d8aaa8f3822fdd9b3d1d33134e266ffdc84
BostoenToby <toby.bostoen@student.howest.be>
2023-03-28 16:56:10 +0200

    modal refactor & add options

--- src/lib/components/Extra/Modal.svelte
+++ src/lib/components/Extra/Modal.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
   import '$lib/styles/table.scss'
-  export let updatePopup: Function, show: boolean
+  export let updatePopup: Function,
+    show: boolean,
+    size: 'small' | 'medium' | 'large' = 'medium'
 </script>
 
 {#if show == true}
-  <section data-component="modal-container">
+  <section data-component="modal-container-{size}">
     <div data-component="modal">
       <button
         data-component="close"
         on:click={() => {
-          updatePopup(null, false)
+          updatePopup(false)
         }}><img src="x.svg" alt="Close button" /></button
       >
-      <div data-component="modal-content"><slot/></div >
+      <div data-component="modal-content"><slot /></div>
     </div>
   </section>
 {/if}
---
+++ src/lib/interfaces/IOption.ts
@@ -0,0 +1,4 @@
+export default interface IOption {
+  name: string
+  value: boolean
+}
