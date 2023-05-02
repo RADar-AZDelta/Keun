fbd63a6c1cafe5c271dbbdc667c64a6be757ddbc
BostoenToby <toby.bostoen@student.howest.be>
2023-05-02 14:57:13 +0200

    Some extra styling

--- src/lib/components/Extra/AthenaActivatedFilter.svelte
+++ src/lib/components/Extra/AthenaActivatedFilter.svelte
@@ -9,20 +9,22 @@
   }
 </script>
 
-<button
-  data-component="filter-button"
-  on:click={showCategories}
-  class={`${openedFilter == filterName ? 'border-radius-top' : null}`}
->
-  <p>{filterName}</p>
-  <SvgIcon href="icons.svg" id="updown" width="16px" height="16px" />
-</button>
-{#if openedFilter == filterName}
-  <div data-component="filter-item" class={checkForScroll(Array.from(filters.values()), 3)}>
-    {#each [...filters] as [filter, options]}
-      {#each options as option}
-        <slot name="option" {filter} {option} />
+<div data-name="filter">
+  <button
+    data-component="filter-button"
+    on:click={showCategories}
+    class={`${openedFilter == filterName ? 'border-radius-top' : null}`}
+  >
+    <p>{filterName}</p>
+    <SvgIcon href="icons.svg" id="updown" width="16px" height="16px" />
+  </button>
+  {#if openedFilter == filterName}
+    <div data-component="filter-item" class={checkForScroll(Array.from(filters.values()), 3)}>
+      {#each [...filters] as [filter, options]}
+        {#each options as option}
+          <slot name="option" {filter} {option} />
+        {/each}
       {/each}
-    {/each}
-  </div>
-{/if}
+    </div>
+  {/if}
+</div>
