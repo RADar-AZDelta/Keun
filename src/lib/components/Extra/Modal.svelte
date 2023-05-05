01f28f11c27a6009e614fca20658acddb4e2f37c
BostoenToby <toby.bostoen@student.howest.be>
2023-05-05 15:07:24 +0200

    Functionalities (fade, escape, outclick) for modal

--- src/lib/components/Extra/Modal.svelte
+++ src/lib/components/Extra/Modal.svelte
@@ -2,23 +2,25 @@
   import '$lib/styles/table.scss'
   import { createEventDispatcher } from 'svelte'
   import SvgIcon from './SvgIcon.svelte'
+  import { fade } from 'svelte/transition'
   import type { CustomOptionsEvents } from '../Types'
+  import { clickOutside } from '$lib/actions/clickOutside'
+  import { escapeWithKey } from '$lib/actions/escapeWithKey'
   export let show: boolean,
     size: 'small' | 'medium' | 'large' = 'medium'
 
   const dispatch = createEventDispatcher<CustomOptionsEvents>()
 
-  function onClick() {
+  function close() {
     dispatch('generalVisibilityChanged', { visibility: false })
   }
 </script>
 
 {#if show == true}
-  <section data-component="modal-container-{size}">
-    <div data-component="modal">
-      <button
-        data-component="close"
-        on:click={onClick}><SvgIcon href="icons.svg" id="x" height="16px" width="16px" /></button
+  <section data-component="modal-container-{size}" in:fade out:fade>
+    <div data-component="modal" use:clickOutside on:outClick={close} use:escapeWithKey on:escapeKey={close}>
+      <button data-component="close" on:click={close}
+        ><SvgIcon href="icons.svg" id="x" height="16px" width="16px" /></button
       >
       <div data-component="modal-content"><slot /></div>
     </div>
