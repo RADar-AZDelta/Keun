1343595ecb347c664c2ef92f1490557a2d1e467f
BostoenToby <toby.bostoen@azdelta.be>
2023-06-22 11:20:31 +0200

    edit file authors

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -5,17 +5,14 @@ export const prerender = false
 
 export const actions: Actions = {
   create: async ({ locals, request }) => {
-    console.log('HERE IN CREATE')
-    // if (!locals.userSession?.uid || !locals.userSession?.roles?.includes('admin')) {
-    //   console.error(`'${locals.userSession?.email}' tried to create an user and was not authorised!`)
-    //   throw redirect(302, '/')
-    // }
+    if (!locals.userSession?.uid || !locals.userSession?.roles?.includes('admin')) {
+      console.error(`'${locals.userSession?.email}' tried to create an user and was not authorised!`)
+      throw redirect(302, '/')
+    }
 
     const formData = await request.formData()
     const email = formData.get('email')
     const role = formData.get('role')
-    console.error("EMAIL ", email)
-    console.error("ROLE ", role)
     // let fileNames = formData.get('fileNames')
     if (!email) return fail(422, { error: 'Email is required' })
     if (!role) return fail(422, { error: 'Role is required' })
