153d1a8d8965f22ce1b62fe9eb640ca654f0b6e5
BostoenToby <toby.bostoen@azdelta.be>
2023-07-13 15:00:40 +0200

    dynamic import & create sequence hook method

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
-import { decodeToken } from '$lib/firebaseAdmin.server';
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import type { DecodedIdToken } from 'firebase-admin/auth';
+import { PUBLIC_CLOUD_IMPLEMENTATION } from '$env/static/public'
 
 const securityHeaders = {
     'Cross-Origin-Embedder-Policy': 'require-corp',
@@ -10,40 +11,53 @@ const securityHeaders = {
 
 const authorizedPages = ["/register"]
 
-const sessionHandle: Handle = async ({ event, resolve }) => {
-	const token = event.cookies.get('token') || '';
-	const decodedToken = await decodeToken(token);
-	if (decodedToken) {
-		const { uid, name, email } = decodedToken;
-		const roles: [string] = [decodedToken['role']];
-		event.locals.userSession = { uid, name, email, roles };
-	}
-
-	const response = await resolve(event);
+const setHeaders: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event)
 	// Object.entries(securityHeaders).forEach(([header, value]) => {
 	// 	response.headers.set(header, value)
 	// })
+	return response
+}
 
+const sessionHandle: Handle = async ({ event, resolve }) => {
+	const token = event.cookies.get('token') || '';
+	await import('$lib/firebaseAdmin.server').then(async({ decodeToken }) => {
+		const decodedToken = await decodeToken(token);
+		if (decodedToken) {
+			const { uid, name, email } = decodedToken;
+			const roles: [string] = [decodedToken['role']];
+			event.locals.userSession = { uid, name, email, roles };
+		}
+	})
+
+	const response = await resolve(event);
 	return response;
 };
 
 const authorizationHandle: Handle = async({ event, resolve}) => {
 	if(authorizedPages.includes(event.url.pathname)){
 		const token = event.cookies.get('token') || ''
-		const decodedToken = await decodeToken(token)
-		if(decodedToken) {
-			const { email } = decodedToken
-			if (!decodedToken['role'].includes('Admin')) {
-				console.error(
-					`'${email}' tried to perform an unauthorized action in the page ${event.url.pathname}!`
-				);
-				throw redirect(302, '/');
+		let decodedToken: DecodedIdToken | null = null
+		await import('$lib/firebaseAdmin.server').then(async({ decodeToken }) => {
+			decodedToken = await decodeToken(token)
+			if(decodedToken) {
+				const { email } = decodedToken
+				if (!decodedToken['role'].includes('Admin')) {
+					console.error(
+						`'${email}' tried to perform an unauthorized action in the page ${event.url.pathname}!`
+					);
+					throw redirect(302, '/');
+				}
 			}
-		}
+		})
 	}
 	
 	const response = await resolve(event)
 	return response
 }
 
-export const handle = sequence(sessionHandle, authorizationHandle)
+let handlers: Handle[] = []
+if(PUBLIC_CLOUD_IMPLEMENTATION == "firebase") handlers = [sessionHandle, authorizationHandle, setHeaders]
+else handlers = [setHeaders]
+
+export const handle = sequence(...handlers)
--- src/routes/api/token/+server.ts
+++ src/routes/api/token/+server.ts
@@ -1,21 +1,22 @@
 import { dev } from '$app/environment'
 import { json, type RequestHandler } from '@sveltejs/kit'
-import { decodeToken } from '$lib/firebaseAdmin.server';
 
 export const POST: RequestHandler = async ({ request, cookies }) => {
   const payload = await request.json()
   const token: string = payload.token || ''
-  const decodedToken = await decodeToken(token)
-  if (token) {
-    cookies.set('token', token, {
-      path: '/',
-      httpOnly: true,
-      secure: !dev,
-      expires: decodedToken?.auth_time ? undefined : new Date(decodedToken?.auth_time!)
-    })
-  } else {
-    cookies.delete('token', { path: '/' })
-  }
+  await import('$lib/firebaseAdmin.server').then(async({ decodeToken }) => {
+    const decodedToken = await decodeToken(token)
+    if (token) {
+      cookies.set('token', token, {
+        path: '/',
+        httpOnly: true,
+        secure: !dev,
+        expires: decodedToken?.auth_time ? undefined : new Date(decodedToken?.auth_time!)
+      })
+    } else {
+      cookies.delete('token', { path: '/' })
+    }
+  })
 
   return json({})
 }
