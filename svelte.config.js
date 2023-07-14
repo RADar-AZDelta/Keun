f8af90385daccbe05edf2e9f03c1dccabbc15356
BostoenToby <toby.bostoen@azdelta.be>
2023-07-14 09:13:55 +0200

    set fallback to generate index.html in build

--- svelte.config.js
+++ svelte.config.js
@@ -25,7 +25,7 @@ const config = {
   },
 
   kit: {
-    adapter: process.env.PUBLIC_CLOUD_IMPLEMENTATION == "firebase" ? autoAdapter() : staticAdapter({ strict: false }),
+    adapter: process.env.PUBLIC_CLOUD_IMPLEMENTATION == "firebase" ? autoAdapter() : staticAdapter({ fallback: 'index.html' }),
     paths: {
       base: dev ? '' : '/Keun',
     }
