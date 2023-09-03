27fe13a85ea53a495b5e001cd6447d1ee58a28e3
BostoenToby <toby.bostoen123@gmail.com>
2023-09-03 21:42:52 +0200

    seperate custom concept file per concept file

--- src/lib/components/Types.d.ts
+++ src/lib/components/Types.d.ts
@@ -169,7 +169,7 @@ export interface IFunctionalityImpl {
   getFiles(): Promise<string[] | void>
   getFilesAdmin(): Promise<string[] | void>
   getAllAuthors(): Promise<Record<string, { email: string; files: Record<string, string> }> | void>
-  downloadFile(fileName: string): Promise<void>
+  downloadFile(fileName: string, usagiString?: boolean): Promise<void>
   uploadFile(file: File, authorizedAuthors: string[]): Promise<string[] | void>
   syncSettings(action: 'read' | 'write'): Promise<void>
   readFileFirstTime(fileName: string): Promise<{
@@ -185,5 +185,5 @@ export interface IFunctionalityImpl {
   removeCache(fileName: string): Promise<void>
   checkForCache(fileName: string): Promise<boolean | void>
   getCachedFiles(): Promise<string[] | void>
-  checkCustomConcepts(): Promise<File | void>
+  checkCustomConcepts(name: string): Promise<File | void>
 }
