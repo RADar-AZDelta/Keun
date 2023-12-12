c1281a0f75f254cd6250f74b7836e9c8878a6798
BostoenToby <toby.bostoen123@gmail.com>
2023-12-12 14:37:22 +0100

    config eslint & prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
+import preprocess from 'svelte-preprocess'
 import staticAdapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
