e50f9bd7db593b759bcc6a2b0503d2fae93f1150
BostoenToby <toby.bostoen123@gmail.com>
2024-06-12 10:25:46 +0200

    Fix issue of prompt to change columns now showing

--- src/lib/helpers/FileReader.ts
+++ src/lib/helpers/FileReader.ts
@@ -2,13 +2,12 @@ export default class Reader {
   private static reader = new FileReader()
   static content: string | undefined = undefined
 
-  static async readFileAsText(file: File): Promise<void> {
-    this.reader.onload = this.setResult
-    this.reader.readAsText(file)
-  }
-
-  static setResult() {
-    if (!this.reader?.result) return (this.content = undefined)
-    this.content = this.reader.result.toString()
+  static async readFileAsText(file: File): Promise<string | ArrayBuffer | null> {
+    return new Promise(resolve => {
+      this.reader.onload = function (this: FileReader) {
+        return resolve(this.result)
+      }
+      this.reader.readAsText(file)
+    })
   }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -22,7 +22,7 @@ const config = {
   kit: {
     adapter: staticAdapter({ fallback: 'index.html', pages: 'public', assets: 'public', precompress: false }),
     paths: {
-      base: process.argv.includes('dev') ? '' : '/Keun'
+      base: process.argv.includes('dev') ? '' : '/Keun',
     },
     alias: {
       $lib: 'src/lib',
